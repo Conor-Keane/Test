8be5bfbc907d1ae15cc32a210a97fa0e972d93bf
conor-keane <G00391846@atu.ie>
2023-10-24 16:21:16 +0100

    Created project

---
+++ src/main/java/ie/atu/testexample1/testexample1/TestExample1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.testexample1.testexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestExample1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestExample1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/testexample1/testexample1/TestExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.testexample1.testexample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestExample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
